09bdf9029bd7b910b353ea7c24a789023a781c95
DanDeLuna <danieldeluna4@gmail.com>
2023-07-20 19:50:06 -0600

    type defs schema

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -16,6 +16,26 @@ const typeDefs = gql`
     image: String
     link: String
   }
-  
+  type Auth {
+    token: ID
+    user: User
+  }
+  type Query {
+    me: User
+  }
+  type Mutation {
+    addUser(username: String, email: String, password: String): Auth
+    login(email: String, password: String): Auth
+    saveBook(
+      bookId: String
+      authors: [String]
+      description: String
+      title: String
+      image: String
+      link: String
+    ): User
+    removeBook(bookId: String): User
+  }
+`;
 
 module.exports = typeDefs;
