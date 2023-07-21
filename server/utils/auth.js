7841fc9b0b5b6e4eb33323ef41ace1ea10a4eef0
DanDeLuna <danieldeluna4@gmail.com>
2023-07-20 19:39:49 -0600

    Auth JS file

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+const secret = process.env.JWT_SECRET;
 const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.query.token || req.headers.authorization || req.body.token;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
