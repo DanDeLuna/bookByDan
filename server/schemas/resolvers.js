df5e2fdd08db31669aa9fb6981d3c7aee3b8a01d
DanDeLuna <danieldeluna4@gmail.com>
2023-07-20 19:47:54 -0600

    Resolver schema

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,74 @@
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const results = User.findOne({ _id: context.user._id });
+        return results;
+      }
+      throw new AuthenticationError("need to login!");
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user with this email");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Wrong user and password");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (
+      parent,
+      { bookId, authors, description, title, image, link },
+      context
+    ) => {
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: context.user._id },
+          {
+            $addToSet: {
+              savedBooks: { bookId, authors, description, title, image, link },
+            },
+          },
+          { new: true, runValidators: true }
+        );
+      } else {
+        throw new AuthenticationError("need to login!");
+      }
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true, runValidators: true }
+        );
+      } else {
+        throw new AuthenticationError("need to login!");
+      }
+    },
+  },
+};
+
+module.exports = resolvers;
