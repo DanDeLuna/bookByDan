0ca6f8d279c10b6e3a052ca59dbcdcf9497ab1e6
DanDeLuna <danieldeluna4@gmail.com>
2023-07-20 21:10:04 -0600

    save book

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,15 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from "react";
 import {
+  Jumbotron,
   Container,
+  CardColumns,
   Card,
   Button,
-  Row,
-  Col
-} from 'react-bootstrap';
+} from "react-bootstrap";
+import { useMutation, useQuery } from "@apollo/client";
+import { GET_ME } from "../utils/queries";
+import Auth from "../utils/auth";
+import { removeBookId } from "../utils/localStorage";
+import { REMOVE_BOOK } from "../utils/mutations";
 
-import { getMe, deleteBook } from '../utils/API';
-import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
